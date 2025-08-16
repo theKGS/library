6b1d9d36893c37d26b495342f33fcebf1bf503f8
KGS <1698415+theKGS@users.noreply.github.com>
2025-08-16 15:39:18 +0200

    Add style sheet

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function Book(author, title, pages, read){
 }
 
 let library = [];
-library.push(new Book("bob", "Smith", 253, true));
-console.log(library);
+library.push(new Book("Bob Smith", "Goldstein", 253, true));
+library.push(new Book("Terry Quizzlepoint", "The Drop", 192, false));
+library.push(new Book("Harry Purple", "The Third Squatter", 120, true));
 
+console.log(library);
