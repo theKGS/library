faa3c965d8b6b14d26cae92201eddb4a851be446
KGS <1698415+theKGS@users.noreply.github.com>
2025-08-17 20:20:34 +0200

    Add update method with helpers to display the library

--- script.js
+++ script.js
@@ -8,23 +8,41 @@ function Book(author, title, pages, read) {
 
 let library = [];
 
+// Replace the current HTML items with a set of new items
 function updateGrid() {
     let children = library.map(makeNodes);
-
     const inp = document.querySelector("ul");
-    console.log(children);
     inp.replaceChildren(...children);
 }
 
+// Convert list of books to list of elements
 function makeNodes(book) {
-    const newElement = document.createElement('p');
-    const newContent = document.createTextNode(`${book.title}`);
-    newElement.appendChild(newContent);
-    console.log(book.title);
+    const newElement = document.createElement('li');
+    wrapString(newElement, "Title: ", book.title);
+    wrapString(newElement, "Author: ", book.author);
+    wrapString(newElement, "Page count: ", book.pages);
+    wrapString(newElement, "Read? ", book.read);
     return newElement;
 }
 
-library.push(new Book("Bob Smith", "Goldstein", 253, true));
+// Produce a complete item line with a bolded label
+function makeLabelledLine(str) {
+    const tagBolded = document.createElement('b');
+    tagBolded.appendChild(document.createTextNode(`${str}`));
+    return tagBolded;
+}
+
+// Produce a single book item
+function wrapString(element, label, data) {
+    const newElementBold1 = makeLabelledLine(label);
+    const newElementTitle = document.createTextNode(`${data}`);
+    element.appendChild(newElementBold1);
+    element.appendChild(newElementTitle);
+    element.appendChild(document.createElement("br"));
+}
+
+
+library.push(new Book("Bob Smith", "Goldstein's Gastrointestinal Garden", 253, true));
 library.push(new Book("Terry Quizzlepoint", "The Drop", 192, false));
 library.push(new Book("Harry Purple", "The Third Squatter", 120, true));
 
